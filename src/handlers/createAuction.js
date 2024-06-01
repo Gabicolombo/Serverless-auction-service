e0dcc6d23f58ddc0c31f255d30a3f1349e492052
Gabriela Colombo <65355183+Gabicolombo@users.noreply.github.com>
2024-06-01 17:41:06 -0300

    Create plugins and set up IAM role to connect with DynamoDB

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,12 +1,23 @@
+const { v4: uuid } = require('uuid');
+const AWS = require('aws-sdk');
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
 
   const { title } = JSON.parse(event.body);
 
   const auction = {
+    id: uuid(),
     title,
     status: 'open',
     createdAt: new Date().toISOString()
   };
+
+  await dynamodb.put({
+    TableName: 'AuctionsTable',
+    Item: auction
+  }).promise();
 
   return {
     statusCode: 201,
